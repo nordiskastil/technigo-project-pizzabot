f5a85484c816b5e603cc15b5856e710d731aa90c
Nathalie <68371736+nordiskastil@users.noreply.github.com>
2025-03-15 14:59:36 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -3,11 +3,19 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to our Javascript Pizzeria. Ready to order? - Click 'OK' to proceed.`
 )
+
+let person = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hi " + person + "welcome!";
+}
 
 // Step 2 - Food choice
 // Your code goes here
+
 
 // Step 3 - Subtype choice
 // Your code goes here
