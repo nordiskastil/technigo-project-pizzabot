579582f7151a63aeec7680b95d531efc7ba0fd8c
Nathalie <68371736+nordiskastil@users.noreply.github.com>
2025-03-15 15:15:39 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -14,7 +14,19 @@ if (person != null) {
 }
 
 // Step 2 - Food choice
-// Your code goes here
+let text;
+let orderFood = prompt("What would you like to eat?");
+switch(orderFood) {
+  case "Pasta carbonara with extra bacon":
+    text = "Good choice!";
+    break;
+  case "Spagetti bolognese":
+    text = "A specialty of the house!";
+    break;
+  case "Pizza margarita":
+    text = "Simply good!";
+    break;
+}
 
 
 // Step 3 - Subtype choice
